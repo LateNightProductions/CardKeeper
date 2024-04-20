17f38cbae2535bacb989e100d1a6878361c0e173
Alex Scherb <awscherb@users.noreply.github.com>
2024-04-20 16:00:20 -0400

    background on header preview

--- app/src/main/java/com/awscherb/cardkeeper/ui/items/PassItem.kt
+++ app/src/main/java/com/awscherb/cardkeeper/ui/items/PassItem.kt
@@ -1,12 +1,20 @@
 package com.awscherb.cardkeeper.ui.items
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.awscherb.cardkeeper.pkpass.model.FieldObject
 import com.awscherb.cardkeeper.pkpass.model.PkPassModel
 import com.awscherb.cardkeeper.pkpass.model.parseHexColor
@@ -26,7 +34,21 @@ fun PassItem(
             containerColor = Color(pass.backgroundColor.parseHexColor())
         )
     ) {
-        PkPassHeaderView(pass = pass)
+        Box {
+            pass.backgroundPath?.let { background ->
+                AsyncImage(
+                    model = background,
+                    contentDescription = "Background",
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.TopCenter,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .blur(radius = 32.dp)
+                )
+            }
+            PkPassHeaderView(pass = pass)
+        }
     }
 }
 
