d316e4a8a804c10bd81e7683e2b05f5e5d077dae
Alex Scherb <awscherb@users.noreply.github.com>
2025-04-19 21:51:30 -0400

    bump deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.work.runtime.ktx)
